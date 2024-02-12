810ed06654a55d64d6a4bb753396d03b1fbde048
mark <markossebhat21@gmail.com>
2024-02-12 21:00:25 +0200

    Update env for deployment

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,26 +1,39 @@
 import pgPromise from 'pg-promise';
 
 const pgp = pgPromise();
+
 const connection = {
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  database: process.env.DB_NAME || 'bookstore',
-  user: process.env.DB_USER || 'your_username',
-  password: process.env.DB_PASSWORD || 'your_password',
+  connectionString: process.env.DATABASE_URL || '',
+  ssl: process.env.NODE_ENV === 'production', // Enable SSL in production on Glitch
 };
-
-
 
 const db = pgp(connection);
 
-// Function to connect to the database
+// Function to connect to the database with retry
 export const connectDatabase = async () => {
-  try {
-    await db.connect();
-    console.log('Connected to the database');
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-    throw error;
+  const maxRetries = 3;
+  let retryCount = 0;
+
+  while (retryCount < maxRetries) {
+    try {
+      await db.connect();
+      console.log('Connected to the database');
+      return; // Connection successful, exit the function
+    } catch (error) {
+      retryCount++;
+      console.error(`Error connecting to the database (Attempt ${retryCount}/${maxRetries}):`, error);
+
+      if (retryCount < maxRetries) {
+        // Wait for a moment before retrying (you can adjust the delay)
+        const delayMs = 1000;
+        console.log(`Retrying in ${delayMs / 1000} seconds...`);
+        await new Promise(resolve => setTimeout(resolve, delayMs));
+      } else {
+        // Maximum retries reached, throw the error
+        console.error('Max retry attempts reached. Unable to connect to the database.');
+        throw error;
+      }
+    }
   }
 };
 
